81c7de7ae32654c2f9909af5b76ff5fb9e835c35
Michał Kozłowski <michalandrzejkozlowski@gmail.com>
2017-12-05 22:32:40 +0100

    rest of vue.js files

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <h1>to do list <i class="fa fa-check-square-o"></i></h1>
+        <app-new></app-new>      
+        <app-list></app-list>
+    </div>
+</template>
+
+<script>
+    import New from './components/New.vue';
+    import List from './components/List.vue';
+
+    export default {
+        components: {
+            appNew: New,
+            appList: List
+        }
+    }
+</script>
+
+<style>
+    body {
+        padding: 30px;
+    }
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,103 @@
+<template>
+	<div>
+		<ul class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4" >
+			<transition-group name="animation" mode="out-in">
+				<li v-for="(todo, index) in todos" 
+					:class="{inactive: todo.inactive}" 
+					@click="active(index)" 
+					:key="index">
+						{{todo.text}}			
+						<i class='fa fa-check' @click.stop="complete(index)"></i>
+				</li>
+			</transition-group>
+		</ul>
+		<h2 class="col-xs-12">Done:</h2>
+		<ul class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4">
+			<transition-group name="animation" mode="out-in">
+				<li v-for="(done, index) in dones" 
+					class="done" 
+					@click="notComplete(index)" 
+					:key="index">
+						{{done.text}}			
+						<i class='fa fa-trash' @click.stop="remove(index)"></i>
+				</li>
+			</transition-group>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		computed: {
+			todos(){
+				return this.$store.getters.todos
+			},
+			dones(){
+				return this.$store.getters.dones
+			}
+		},
+		methods: {
+			active(index) {
+				this.$store.dispatch('active', index);
+			},
+			complete(index) {
+				this.$store.dispatch('complete', index);
+			},
+			notComplete(index) {
+				this.$store.dispatch('notComplete', index);
+			},
+			remove(index) {
+				this.$store.dispatch('remove', index);
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	ul {
+		padding: 0;
+		margin-top: 30px;
+	}
+
+	h2 {
+		margin-top: 80px;
+	}
+
+	.inactive {
+		text-decoration: line-through;
+		color: #999;
+	}
+
+	.done {
+		color: #999;
+	}
+
+	.animation-enter-active {
+		animation: bounce-in .5s;
+	}
+
+	.animation-leave-active {
+		animation: fade-out 300ms forwards;
+	}
+
+	@keyframes bounce-in {
+		0% {
+			transform: scale(0);
+		}
+		50% {
+			transform: scale(1.5);
+		}
+		100% {
+    		transform: scale(1);
+		}
+	}
+
+    @keyframes fade-out {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0;
+        }
+    }
+</style>
