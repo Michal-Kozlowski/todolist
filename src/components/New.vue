a9d11a8ab544976015d23ac00f396cf2fd24d143
Michał Kozłowski <michalandrzejkozlowski@gmail.com>
2018-02-19 15:35:28 +0100

    unified tabs and spaces

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,30 +1,30 @@
 <template>
-	<div>
-		<input 
-			class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4 input" 
-			type="text" 
-			v-model="todo"
-			autofocus
-			@keydown.enter="addTodo" 
-			placeholder="What to do?">
-	</div>
+    <div>
+        <input 
+            class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4 input" 
+            type="text" 
+            v-model="todo"
+            autofocus
+            @keydown.enter="addTodo" 
+            placeholder="What to do?">
+    </div>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				todo: ''
-			}
-		},
-		methods: {
-			addTodo() {
-				if(this.todo){
-					const newTodo = { text: this.todo, inactive: false, done: false };
-					this.$store.dispatch('addTodo', newTodo);
-					this.todo = '';
-				}				
-			}
-		}
-	}
+    export default {
+        data() {
+            return {
+                todo: ''
+            }
+        },
+        methods: {
+            addTodo() {
+                if(this.todo){
+                    const newTodo = { text: this.todo, inactive: false, done: false };
+                    this.$store.dispatch('addTodo', newTodo);
+                    this.todo = '';
+                }               
+            }
+        }
+    }
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -4,59 +4,59 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {
-		todos: [
-			{text: "This has to be done", inactive: false},
-			{text: "...and this too", inactive: false},
-			{text: "maybe not this", inactive: true}			
-		],
-		dones: [
-			{text: "This one is done", inactive: false}
-		]
-	},
-	mutations: {
-		ADD_TODO(state, todo){
-			state.todos.push(todo);
-		},
-		ACTIVE(state, index){
-			state.todos[index].inactive = !state.todos[index].inactive;
-		},
-		COMPLETE(state, index){
-			state.todos[index].inactive = false;
-			state.dones.push(state.todos[index]);
-			state.todos.splice(index, 1);
-		},
-		NOT_COMPLETE(state, index){
-			state.todos.push(state.dones[index]);
-			state.dones.splice(index, 1);
-		},
-		REMOVE(state, index){
-			state.dones.splice(index, 1);
-		}
-	},
-	actions: {
-		addTodo({commit}, todo){
-			commit('ADD_TODO', todo)
-		},
-		active({commit}, index){
-			commit('ACTIVE', index)
-		},
-		complete({commit}, index){
-			commit('COMPLETE', index)
-		},
-		notComplete({commit}, index){
-			commit('NOT_COMPLETE', index)
-		},
-		remove({commit}, index){
-			commit('REMOVE', index)
-		}
-	},
-	getters: {
-		todos: state => {
-			return state.todos;
-		},
-		dones: state => {
-			return state.dones;
-		}
-	}
-});	
+    state: {
+        todos: [
+            {text: "This has to be done", inactive: false},
+            {text: "...and this too", inactive: false},
+            {text: "maybe not this", inactive: true}            
+        ],
+        dones: [
+            {text: "This one is done", inactive: false}
+        ]
+    },
+    mutations: {
+        ADD_TODO(state, todo){
+            state.todos.push(todo);
+        },
+        ACTIVE(state, index){
+            state.todos[index].inactive = !state.todos[index].inactive;
+        },
+        COMPLETE(state, index){
+            state.todos[index].inactive = false;
+            state.dones.push(state.todos[index]);
+            state.todos.splice(index, 1);
+        },
+        NOT_COMPLETE(state, index){
+            state.todos.push(state.dones[index]);
+            state.dones.splice(index, 1);
+        },
+        REMOVE(state, index){
+            state.dones.splice(index, 1);
+        }
+    },
+    actions: {
+        addTodo({commit}, todo){
+            commit('ADD_TODO', todo)
+        },
+        active({commit}, index){
+            commit('ACTIVE', index)
+        },
+        complete({commit}, index){
+            commit('COMPLETE', index)
+        },
+        notComplete({commit}, index){
+            commit('NOT_COMPLETE', index)
+        },
+        remove({commit}, index){
+            commit('REMOVE', index)
+        }
+    },
+    getters: {
+        todos: state => {
+            return state.todos;
+        },
+        dones: state => {
+            return state.dones;
+        }
+    }
+}); 
